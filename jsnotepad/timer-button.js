9ffc13eb9c97c5bc9bc6321409c7a9f57348df0a
liuyanyansun <1694614518@qq.com>
2020-04-09 09:22:49 +0800

    message

---
+++ jsnotepad/timer-button.js
@@ -0,0 +1,38 @@
+var $dlgGoto = (function () {
+    var html = '<div class="notepad-gld-goto">'
+        + '<div class="dialogbox">'
+        + '<div class="titlebar">'
+        + '<p class="title">转到指定行</p>'
+        + '<span class="close-btn">X</span>'
+        + '</div>'
+        + '<div class="main">'
+        + '<label>行号(L)</label>'
+        + '<br>'
+        + '<input class="txt-line-num" type="text" autofocus>'
+        + '<br>'
+        + '<input class="btn-goto" type="button" value="转到">'
+        + '<input class="btn-cancel" type="button" value="取消">'
+        + '</div>'
+        + '</div>'
+        + '</div>',
+
+        $dlg = $(html),
+        cfg = {
+            container: 'body',
+            num = 6,
+            title: '同意',
+            onClick: null
+        };
+
+    function show(conf) {
+        //1.DOM draw
+        $(cfg.container).append($dlg);
+        $.extend(cfg, conf);
+        //2.event bind
+        $btn.click(cfg.onClick);
+
+    }
+    return {
+        show: show
+    };
+}());
